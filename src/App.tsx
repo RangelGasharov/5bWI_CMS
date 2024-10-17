a87d111728da0f9be47fa414a0ed49aac1c6c6db
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-10-17 22:47:28 +0200

    Added design changes to the Card component and the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import './App.css'
 import Card from './components/Card'
 
+let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 function App() {
   return (
-    <div className='bg-drab-dark-brown h-[100vh]'>
-      <Card
-        imageURL='https://images.unsplash.com/photo-1728853487293-1a4c5c39b393?q=80&w=3384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        textTitle='Lorem ipsum dolor sit amet'
-        descriptionText='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    <div className='bg-drab-dark-brown'>
+      <div className='p-10'>
+        <div className='text-white text-4xl font-bold text-right'>HTL Dornbirn 5bWI</div>
+      </div>
+      <div className='grid sm:grid-cols-2 md:grid-cols-2, lg:grid-cols-4 2xl:grid-cols-6 gap-x-4 gap-y-6 p-10 justify-items-center'>
+        {ids.map((id) => {
+          return (
+            <Card
+              key={id}
+              imageURL='https://images.unsplash.com/photo-1728853487293-1a4c5c39b393?q=80&w=3384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              textTitle='Lorem ipsum dolor sit amet'
+              descriptionText='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
-      />
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,12 +7,12 @@ type CardType = {
 
 export default function Card({ imageURL, imageAlternativeDescription = "image", textTitle, descriptionText }: CardType) {
     return (
-        <div className="bg-slate-200 rounded-2xl overflow-hidden">
-            <div className="size-64">
-                <img src={imageURL} alt={imageAlternativeDescription} />
-            </div>
+        <div className="bg-slate-200 rounded-2xl overflow-hidden max-w-64">
             <div>
-                <div>{textTitle}</div>
+                <img className="object-scale-down overflow-hidden" src={imageURL} alt={imageAlternativeDescription} />
+            </div>
+            <div className="p-6 text-xs">
+                <div className="font-bold">{textTitle}</div>
                 <div>{descriptionText}</div>
             </div>
         </div>
