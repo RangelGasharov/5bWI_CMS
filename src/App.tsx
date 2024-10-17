ff21e3c176e5ef919bf504634594afbd677936b5
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-10-17 20:19:33 +0200

    Added CSS reset, added a customer color to the tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import Card from './components/Card'
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-      <div className='bg-red-100'>Hello World</div>
+    <div className='bg-drab-dark-brown h-[100vh]'>
       <Card
         imageURL='https://images.unsplash.com/photo-1728853487293-1a4c5c39b393?q=80&w=3384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         textTitle='Lorem ipsum dolor sit amet'
@@ -14,7 +12,7 @@ function App() {
         consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
       />
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "drab-dark-brown": "#2D2B03"
+      }
+    },
   },
   plugins: [],
 }
