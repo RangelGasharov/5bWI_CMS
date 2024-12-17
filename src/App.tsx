83d777242c4ee21c758d78b03114148f1b811184
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-12-17 22:42:11 +0100

    Refactoring - added subject type and made minor changes to ensure that build is excecuted correctly

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { createClient } from '@supabase/supabase-js';
 import './App.css'
 import { useEffect, useState } from 'react';
 
+type SubjectType = {
+  id: number,
+  name: string
+}
+
 const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
 
 function App() {
@@ -13,13 +18,13 @@ function App() {
   }, []);
 
   async function getSubjects() {
-    const { data } = await supabase.from("subject").select();
-    setSubjects(data);
+    const subjects: any = await supabase.from("subject").select();
+    setSubjects(subjects);
   }
 
   return (
     <ul>
-      {subjects?.map((subject) => (
+      {subjects?.map((subject: SubjectType) => (
         <div key={subject.id}>{subject.name}</div>
       ))}
     </ul>
