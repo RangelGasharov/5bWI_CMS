7c67644fb2920ad15c0b5a51715b5b3f5d365859
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-12-17 22:33:08 +0100

    Added database connection to supabase, added env files to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
+import { createClient } from '@supabase/supabase-js';
 import './App.css'
-import CardContainer from './components/CardContainer'
+import { useEffect, useState } from 'react';
 
-type Project = {
-  id: number,
-  projectTitle: string,
-  projectDescription: string,
-  projectImageURL: string
-}
-
-let cardsData = fetch("http://10.115.1.36:8055/items/Projects").then(response => {
-  if (!response.ok) {
-    throw new Error("Network response was not ok" + response.statusText);
-  }
-  return response.json();
-}).catch(error => {
-  console.error("Error fetching data:", error);
-})
+const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
 
 function App() {
+
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    getSubjects();
+  }, []);
+
+  async function getSubjects() {
+    const { data } = await supabase.from("subject").select();
+    setSubjects(data);
+  }
+
   return (
-    <div className='bg-drab-dark-brown min-h-[100vh]'>
-      <div className='p-10'>
-        <div className='text-white text-4xl font-bold text-right'>HTL Dornbirn 5bWI</div>
-      </div>
-      <CardContainer
-        cardsData={cardsData} />
-    </div>
+    <ul>
+      {subjects?.map((subject) => (
+        <div key={subject.id}>{subject.name}</div>
+      ))}
+    </ul>
   )
 }
 
